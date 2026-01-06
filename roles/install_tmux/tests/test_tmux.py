e34e266a7d84da685a97d3605fcb29b39a206fc3
Houston Fortney <hfortney@path-robotics.com>
2026-01-05 22:17:32 -0500

    Switch to tpm

--- roles/install_tmux/tests/test_tmux.py
+++ roles/install_tmux/tests/test_tmux.py
@@ -8,3 +8,10 @@ def test_config_is_populated(host):
     config_path = f"{user.home}/.tmux.conf"
     config_file = host.file(config_path)
     assert config_file.exists, f"tmux config file {config_path} should exist"
+
+
+def test_tpm_is_installed(host):
+    user = host.user()
+    tpm_path = f"{user.home}/.tmux/plugins/tpm"
+    tpm_dir = host.file(tpm_path)
+    assert tpm_dir.exists, f"tmux plugin manager should be installed at {tpm_path}"
