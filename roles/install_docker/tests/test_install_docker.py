47f1cddac6dced2e06392c665d8152d8a997b8b1
Houston Fortney <hfortney@path-robotics.com>
2026-02-16 23:13:10 -0500

    Skip docker role in molecule

--- roles/install_docker/tests/test_install_docker.py
+++
@@ -1,3 +0,0 @@
-def test_docker_installation(host):
-    result = host.run("sg docker -c 'docker run --rm hello-world'")
-    assert result.rc == 0, "Docker is not installed or not functioning properly"
